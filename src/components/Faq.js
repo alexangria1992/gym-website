2857ba6a600e1a9cffbe37daff94a0c874a65997
alexangria1992 <alex040892@gmail.com>
2023-06-13 23:11:28 +1200

    Join Section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -11,7 +11,7 @@ const Faq = () => {
     <section className="section pt-[480px]  lg:pt-[280px]">
       <div
         className="max-w-[768px] mx-auto lg:big-faq bg-no-repeat bg-custom bg-center 
-      lg:h-[1160px]  lg:pt-6 "
+      lg:h-[1160px]  lg:pt-6 mb-[80px] lg:mb-0"
       >
         <div
           className="section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px]  
