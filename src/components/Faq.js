b64f4d8d04f25bb66e12d6300fe3c736fd87d785
alexangria1992 <alex040892@gmail.com>
2023-06-13 22:43:28 +1200

    Faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React from "react";
+
+// import data
+import { faq } from "../data";
+
+import Accordion from "../components/Accordion";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  const { icon, title, accordions } = faq;
+  return (
+    <section className="section pt-[480px]  lg:pt-[280px]">
+      <div
+        className="max-w-[768px] mx-auto lg:big-faq bg-no-repeat bg-custom bg-center 
+      lg:h-[1160px]  lg:pt-6 "
+      >
+        <div
+          className="section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px]  
+        px-4 mx-auto lg:px-0"
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-delay="200"
+        >
+          <img src={icon} className="lg:hidden" alt="" />
+          <h2 className="h2 section-title lg:mt-[100px]">
+            {title} <span className="text-primary-200">.</span>
+          </h2>
+        </div>
+        {/* accordion list */}
+        <div
+          className="flex flex-col gap-y-4 px-4"
+          data-aos="fade-up"
+          data-aos-offset="300"
+          data-aos-delay="200"
+        >
+          {accordions.map((accordion, idx) => {
+            return <Accordion accordion={accordion} key={idx} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Faq;
